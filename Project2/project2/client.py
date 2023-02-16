68a39ebfc393bad0a2bb274774287eb6c7a52d30
Kenany02 <124407206+Kenany02@users.noreply.github.com>
2023-02-15 22:39:04 -0500

    Update client.py

--- Project2/project2/client.py
+++ Project2/project2/client.py
@@ -2,9 +2,7 @@ import socket
 
 
 def client():
-    # Define ports for reverse and upper servers
-    port = 50007
-    localhost_addr = socket.gethostbyname(socket.gethostname())
+
 
     try:
         csr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,17 +11,17 @@ def client():
         print('socket open error: {} \n'.format(err))
         exit()
 
-    # connect to the reverse string server on local machine
+    # connect to to the LS
     server_binding = (localhost_addr, port)
     csr.connect(server_binding)
 
     write_lines_from_server(csr, 'PROJ2-HNS.txt', 'RESOLVED.txt')
 
-    # close the reverse client socket
+    # close the client socket
     csr.close()
     
 
-#method to read lines from input file, send to server, and write output to out_path
+#method to read lines from input, send to server, and back to client and to output file
 def write_lines_from_server(sock, in_path, out_path):
     lines = []
     with open(in_path, 'r') as in_file, open(out_path, 'w') as out_file:
@@ -33,5 +31,9 @@ def write_lines_from_server(sock, in_path, out_path):
             out_file.write(data_from_server.decode('utf-8').strip() + '\n')
 
 if __name__ == "__main__":
+#Arguments for port and localhost address    
+    port = int(sys.argv[1])
+    localhost_addr = (sys.argv[2])
     client()
+    
 
