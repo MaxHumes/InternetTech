d0636ce40ae2a869225a8054a0baa0300aa98e3f
MaxHumes <maxhumes@live.com>
2023-02-15 23:07:12 -0500

    Initial work on LS
    Full implementation of load balancing server, still needs to be tested and probably doesn't work.

---
+++ Project2/project2/ls.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import select
+
+def ls():
+    #create ls socket
+    try:
+        lssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[ls]: Load balancing server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n'.format(err))
+        exit()
+    #listen for client connection
+    server_binding = ('', int(sys.argv[1]))
+    lssock.bind(server_binding)
+    lssock.listen(1)
+    csockid, addr = lssock.accept()
+    print ("[ls]: Got a connection request from a client at {}".format(addr))
+
+
+    ts1sock = get_ts_socket(sys.argv[2], sys.argv[3])
+    ts2sock = get_ts_socket(sys.argv[4], sys.argv[5])
+    print("[ls]: Connected to TS servers")
+
+    csockid.setblocking(0)
+    while True:
+        #recieve domain name from client
+        clientBytes = csockid.recv(200)
+        if len(clientBytes) == 0:
+            break
+
+        #send bytes to TS1
+        ts1sock.send(clientBytes)
+        ts1data = ts1sock.recv(200)
+        #send bytes to TS2
+        ts2sock.send(clientBytes)
+        ts2data = ts2.recv(200)
+
+        #block with select and wait for timeout
+        if all(not l for l in select.select([ts1sock, ts2sock],[],[],5)):
+            #timeout
+            timeout_str = '{} - TIMED OUT'.format(clientBytes.decode('utf-8'))
+            csockid.send(timeout_str.encode('utf-8'))
+        else:
+            #return domain name
+            if len(ts1data) > 0:
+                csockid.send(ts1data)
+            else:
+                csockid.sent(ts2data)
+
+
+
+    # Close the server socket
+    print ("[ls]: Closing load balancing server socket {}".format(host))
+    ss.close()
+    exit()
+
+def get_ts_socket(host_name, listen_port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[ls]: TS client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+
+    # connect to the reverse string server on local machine
+    server_binding = (host_name, listen_port)
+    sock.connect(server_binding)
+
+    return sock
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 6:
+        print('Error: Not enough arguments provided')
+        exit()
+    ls()
